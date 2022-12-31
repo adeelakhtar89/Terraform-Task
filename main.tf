942a5ed576ddba079c908b572706a2ad55eeb39c
adeelakhtar89 <89094288+adeelakhtar89@users.noreply.github.com>
2022-12-31 19:29:05 +0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,93 @@
+# Create VPC
+
+resource "aws_vpc" "vpc" {
+  cidr_block              = "${var.vpc-cidr}"
+  instance_tenancy        = "default"
+  enable_dns_hostnames    = true
+
+  tags      = {
+    Name    = "Test VPC"
+  }
+}
+
+#Internet Gateway and Attach it to VPC
+
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id    = aws_vpc.vpc.id
+
+  tags      = {
+    Name    = "Test IGW"
+  }
+}
+
+
+#Route Table and Add Public Route
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id       = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
+  }
+
+  tags       = {
+    Name     = "Public Route Table"
+  }
+}
+
+# Associate Public Subnet 1 to "Public Route Table"
+
+resource "aws_route_table_association" "public-subnet-1-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-1.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+
+# Associate Public Subnet 2 to "Public Route Table"
+
+resource "aws_route_table_association" "public-subnet-2-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-2.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+
+
+
+
+# Creating Nat Gateway
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+  depends_on    = [aws_internet_gateway.internet-gateway]
+}
+
+ #Route Table and Add Private Route
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id       = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat-gw.id
+  }
+
+}
+
+# Associate Private Subnet 1 to "Private Route Table"
+
+resource "aws_route_table_association" "private-subnet-1-route-table-association" {
+  subnet_id           = aws_subnet.private-subnet-1.id
+  route_table_id      = aws_route_table.private-route-table.id
+}
+
+# Associate Private Subnet 2 to "Private Route Table"
+
+resource "aws_route_table_association" "private-subnet-2-route-table-association" {
+  subnet_id           = aws_subnet.private-subnet-2.id
+  route_table_id      = aws_route_table.private-route-table.id
+  
+ }
+
